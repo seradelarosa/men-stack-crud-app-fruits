7333ee16f95a36b8afcb838db926c188b2948e20
Sera De La Rosa <sera.delarosa@outlook.com>
2025-02-14 12:43:32 -0500

    Configured the app for MongoDB connection.

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
+
+mongoose.connect(process.env.MONGODB_URI);
+
+mongoose.connection.on('connected', () => {
+    console.log(`Connected on MongoDB ${mongoose.connection.name}`);
+});
 
 app.get('/', async (req, res) => {
     res.render('index.ejs');
